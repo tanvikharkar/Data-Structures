5993361e1894a7bf8f2a40dec1c82a6358b43108
Tanvi Kharkar <tanviykharkar@gmail.com>
2024-02-21 15:50:48 -0800

    Add files via upload

---
+++ Queue/queue_linked.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self,item):
+        self.item = item
+        self.next = None    # reference to next element in linked list
+
+class Queue:
+    '''Implements an linked list based, efficient first-in first-out Queue Data Type'''
+
+    def __init__(self, capacity):
+        '''Creates an empty Queue with a capacity'''
+        self.capacity = capacity
+        self.front = None
+        self.back = None
+        self.num_items = 0
+
+    def is_empty(self):
+        '''Returns True if the Queue is empty, and False otherwise
+           MUST have O(1) performance'''
+        return self.num_items == 0
+
+    def is_full(self):
+        '''Returns True if the Queue is full, and False otherwise
+           MUST have O(1) performance'''
+        return self.num_items == self.capacity
+
+    def enqueue(self, item):
+        '''If Queue is not full, enqueues (adds) item to Queue 
+           If Queue is full when enqueue is attempted, raises IndexError
+           MUST have O(1) performance'''
+        if self.num_items == self.capacity:
+            raise IndexError
+        else:
+            enq_node = Node(item)
+            if self.num_items == 0:
+                self.front = self.back = enq_node
+            else:
+                self.back.next = enq_node   # reference link to enq_node
+                self.back = enq_node        # enq_node enqueued at back of queue
+            self.num_items += 1
+
+    def dequeue(self):
+        '''If Queue is not empty, dequeues (removes) item from Queue and returns item.
+           If Queue is empty when dequeue is attempted, raises IndexError
+           MUST have O(1) performance'''
+        if self.num_items == 0:
+            raise IndexError
+        else:
+            deq_item = self.front.item
+            self.front = self.front.next
+            self.num_items -= 1
+            if self.is_empty():
+                self.front = self.back = None
+            return deq_item
+
+    def size(self):
+        '''Returns the number of elements currently in the Queue, not the capacity
+           MUST have O(1) performance'''
+        return self.num_items
